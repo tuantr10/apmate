aa7bc9f522b32996fa66eb72898d901a7d94661f
Tuan Dung TRAN <mcafee92@gmail.com>
2018-03-22 09:33:41 +0000

    fix encoding for Japanese

--- connect.php
+++ connect.php
@@ -1,9 +1,15 @@
 <?php
 include("settings.php");
 $conn = mysqli_connect(constant('HOST'), constant('USERNAME'), constant('PASSWORD'), constant('DBNAME'));
+
 /* check connection */
 if (mysqli_connect_errno()) {
   printf("Connect failed: %s\n", mysqli_connect_error());
   exit();
+}
+
+if (!mysqli_set_charset($conn, "utf8")) {
+    printf("Error loading character set utf8: %s\n", mysqli_error($conn));
+    exit();
 }
 ?>
--- timetableadvance.php
+++ timetableadvance.php
@@ -1,6 +1,5 @@
 <?php
 session_start();
-//header('Content-Type: text/html; charset=utf-8');
 /**
  * PHP version 5
  *
